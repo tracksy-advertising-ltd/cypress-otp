3e38f62b5aa0c7ee009406fd3cd1b03de7f61c1a
Tom Kühl <hello@tomkuehl.io>
2019-10-02 12:07:57 +0300

    replace otplib with speakeasy

--- src/index.js
+++ src/index.js
@@ -1,21 +1,9 @@
-const otplib = require("otplib");
+const speakeasy = require('speakeasy')
 
-let previousSecret;
-module.exports = (args) => {
-  if(typeof args === 'string' && args) {
-    previousSecret = args;
-  } else if(typeof args === 'object') {
-    previousSecret = args.secret;
-    if(args.options) {
-      otplib.authenticator.options = args.options;
-    }
-  }
-  const secret = previousSecret;
-  if(!secret) {
-    throw new Error("No secret has been provided.");
-  }
 
-  const token = otplib.authenticator.generate(secret)
-  otplib.authenticator.check(token, secret)
-  return token
+module.exports = (secret) => {
+  return speakeasy.totp({
+    secret,
+    encoding: 'base32',
+  })
 }
