b490ac052c9c08287712a94c01a86c5b0acf64e3
Tom Kühl <hello@tomkuehl.io>
2019-10-02 11:09:14 +0300

    Add otplib options

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ const otplib = require("otplib");
 
 let previousSecret;
 
-module.exports = (otpSecret) => {
-  if(otpSecret) {
+module.exports = (args) => {
+  if(typeof args === 'string' && args) {
     previousSecret = otpSecret;
+  } else if(typeof args === 'object') {
+    previousSecret = args.secret;
+    if(args.options) {
+      otplib.authenticator.options = args.options;
+    }
   }
   const secret = previousSecret;
   if(!secret) {
